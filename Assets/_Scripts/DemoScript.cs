8d1eaed52cd889b4dbdbd9a09e0e77f3ae25d7f2
daveguy <davidlbrq@gmail.com>
2017-07-03 12:27:09 -0400

    scale and permute working, need to fix output

--- Assets/_Scripts/DemoScript.cs
+++ Assets/_Scripts/DemoScript.cs
@@ -26,7 +26,7 @@ public class answerRecorder{
 		currentSample++;
 		if (currentSample == responses.Length) {
 			isFinished = true;
-			outputResult ();
+//			outputResult ();
 		}
 	}
 
@@ -54,9 +54,12 @@ public class answerRecorder{
 
 public class DemoScript : MonoBehaviour {
 
-	public GameObject lambertianSurfaceVertical;
-	public GameObject lambertianSurfaceHorizontal;
+	public GameObject surfaceHorizontal;
+	public GameObject surfaceVertical;
 	public float[] distances;
+	public Transform initCameraPos;
+	public float maxPermX;
+	public float maxPermZ;
 //	public float fadeDuration;
 
 	private GameObject currentSurface;
@@ -64,7 +67,6 @@ public class DemoScript : MonoBehaviour {
 	private answerRecorder recorder;
 	private int numSamples = 10;
 	private string filename;
-	private bool waitForFade;
 
 	//variables for scaling
 	private Vector3 initScale;
@@ -74,16 +76,20 @@ public class DemoScript : MonoBehaviour {
 
 	void Start () {
 		filename = string.Format("{0}.csv", System.DateTime.Now.ToString("yyyy-MMM-dd-HH-mm-ss"));
-		System.IO.Directory.CreateDirectory("output/");
-		StreamWriter sw = File.AppendText("output/demo-"+filename);
-		sw.WriteLine("The following is the demo results");
-		sw.Close();
+//		System.IO.Directory.CreateDirectory("output/");
+//		StreamWriter sw = File.AppendText("output/demo-"+filename);
+//		sw.WriteLine("The following is the demo results");
+//		sw.Close();
 
 		deactivateAll ();
-		getNextSurface();
+		resetLights (surfaceVertical);
+		permuteLights (surfaceVertical);
+		resetLights (surfaceHorizontal);
+		permuteLights (surfaceHorizontal);
+		currentSurface = surfaceVertical;
+		currentSurface.SetActive (true); //change this for a method to randomly pick a surface
 		recorder = new answerRecorder (numSamples, filename);
-		waitForFade = false;
-		currentDistanceIndex = distances.Length - 1;
+		currentDistanceIndex = 0;
 
 		//set initial values for scaling
 		initScale = currentSurface.transform.localScale;
@@ -95,25 +101,22 @@ public class DemoScript : MonoBehaviour {
 
 	void Update ()
 	{
-		if (waitForFade) {
-			//do nothing
-		}
-		else if (!recorder.isFinished) {
+		if (!recorder.isFinished) {
 			if (Input.GetKeyDown (KeyCode.LeftControl)) {
 				if (currentSurface.transform.Find ("vertical")) {
 					recorder.recordResponses ("vertical", "horizontal");
 				} else {
 					recorder.recordResponses ("horizontal", "horizontal");
 				}
-				getNextSurface ();
+				swapSurface ();
 			} else if (Input.GetKeyDown (KeyCode.RightControl)) {
 				if (currentSurface.transform.Find ("vertical")) {
 					recorder.recordResponses ("vertical", "vertical");
 				} else {
 					recorder.recordResponses ("horizontal", "vertical");
 				}
-				getNextSurface ();
-			} else if (Input.GetKeyDown (KeyCode.Space)) {
+				swapSurface ();
+			} else if (Input.GetKeyDown (KeyCode.S)) {
 				scale ();
 			}
 		} else {
@@ -126,9 +129,18 @@ public class DemoScript : MonoBehaviour {
 	void scale(){
 		currentDistanceIndex = (currentDistanceIndex + 1) % distances.Length;
 		float scale = distances [currentDistanceIndex] / initZDist;
-		currentSurface.transform.position = new Vector3 (currentSurface.transform.position.x, 1-scale, distances [currentDistanceIndex]);
-		currentSurface.transform.localScale = new Vector3 (initScale.x * scale, initScale.y * scale, initScale.z * scale);
-		Light[] lights = currentSurface.transform.Find ("lights").GetComponentsInChildren<Light> ();
+		scaleSurface (surfaceVertical, scale);
+		scaleSurface (surfaceHorizontal, scale);
+		resetLights (surfaceVertical);
+		permuteLights (surfaceVertical);
+		resetLights (surfaceHorizontal);
+		permuteLights (surfaceHorizontal);
+	}
+
+	void scaleSurface(GameObject surface, float scale){
+		surface.transform.position = new Vector3 (surface.transform.position.x, 1-scale, distances [currentDistanceIndex]);
+		surface.transform.localScale = new Vector3 (initScale.x * scale, initScale.y * scale, initScale.z * scale);
+		Light[] lights = surface.transform.Find ("lights").GetComponentsInChildren<Light> ();
 		foreach (Light l in lights) {
 			if (l.name.Equals ("halo")) {
 				l.range = initHaloScale * scale;
@@ -138,28 +150,43 @@ public class DemoScript : MonoBehaviour {
 		}
 	}
 
-
-	void getNextSurface(){
-//		waitForFade = true;
-//		yield return fade(0);
-		currentSurface = lambertianSurfaceVertical;
-		currentSurface.SetActive (true);
-//		yield return fade(1);
-//		waitForFade = false;
+	void permuteLights(GameObject surface){
+		Transform[] children = surface.GetComponentsInChildren<Transform> ();
+		float xPerm;
+		float zPerm;
+		foreach (Transform t in children) {
+			if(t.name.Equals("Spotlight")){
+				xPerm = Random.Range (-maxPermX, maxPermX);
+				zPerm = Random.Range (-maxPermZ, maxPermZ);
+				t.transform.localPosition = new Vector3 (t.transform.localPosition.x + xPerm, t.transform.localPosition.y, t.transform.localPosition.z + zPerm);
+			}
+		}
 	}
 
-//	IEnumerator fade (float targetAlpha)
-//	{
-//		yield return StartCoroutine(currentSurface.GetComponent<Fade> ().Fade3D (targetAlpha, fadeDuration));
-//	}
+	void resetLights(GameObject surface){
+		Transform[] children = surface.transform.GetComponentsInChildren<Transform> ();
+		foreach (Transform t in children) {
+			if (t.name.Equals ("Spotlight")) {
+				Vector3 initPos = t.Find ("initPosition").localPosition;
+				t.localPosition = new Vector3 (initPos.x, initPos.y, initPos.z);
+			}
+		}
+	}
 
-	void deactivateAll(){
-	
-		lambertianSurfaceVertical.SetActive (false);
-		lambertianSurfaceHorizontal.SetActive (false);
-//		Renderer ren = g.GetComponent<Renderer> ();
-//		ren.sharedMaterial.color = new Color(ren.sharedMaterial.color.r, ren.sharedMaterial.color.g, ren.sharedMaterial.color.b, 0f);
+	void swapSurface(){
+		currentSurface.SetActive (false);
+		if (currentSurface == surfaceHorizontal) {
+			currentSurface = surfaceVertical;
+			initCameraPos.position = new Vector3 (0, 1, 0);
+		} else {
+			currentSurface = surfaceHorizontal;
+			initCameraPos.position = new Vector3 (0, 1, -1000);
+		}
+		currentSurface.SetActive (true);
+	}
 
-		
+	void deactivateAll(){	
+		surfaceVertical.SetActive (false);
+		surfaceHorizontal.SetActive (false);		
 	}
 }
